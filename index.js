73cb42119ce22ba2171f9fa5c96834f6c31877ea
Gourob Saha <gourobsaha009@gmail.com>
2022-10-24 23:09:19 +0600

    API Sent Successfully

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const courses = require('./courses.json');
+
 app.get('/', (req, res) => {
     res.send('Course API Running');
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse);
 });
 
 app.listen(port, () => {
